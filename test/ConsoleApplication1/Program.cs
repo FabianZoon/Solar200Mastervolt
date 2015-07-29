52288d323c5a2035efe2a9587f216b1805d864cb
Fabian Zoon <fabian@diligente.nl>
2015-07-29 15:33:26 +0200

    Add header and some extra fields to the output

--- test/ConsoleApplication1/Program.cs
+++ test/ConsoleApplication1/Program.cs
@@ -12,15 +12,22 @@ namespace ConsoleApplication1
 
         static void Main(string[] args)
         {
-
-            Console.Clear();
             Zon z = new Zon(ConfigurationManager.AppSettings["com"]);
             z.Process();
 
             Console.Write(z.PrintToConsole());
-            System.IO.File.AppendAllText(string.Format(ConfigurationManager.AppSettings["currentfile"], DateTime.Now),
-                String.Format("{0};{1};{2};{3};{4};{5};{6};{7};{8};{9};{10}\r\n", DateTime.Now, z.Huidige.ACPower, z.Huidige.Activity, z.Huidige.mainsVoltage, z.Huidige.PowerOutput, z.Huidige.RunningHours, z.Huidige.solarCurrent, z.Huidige.solarVoltage, z.Huidige.Temperature, z.Huidige.TotalKwh, z.LinkStatus));
+            string filename = string.Format(ConfigurationManager.AppSettings["currentfile"], DateTime.Now);
+            if (!System.IO.File.Exists(filename))
+            {
+                System.IO.File.AppendAllText(filename, "date;ACPower;Activity;mainsVolt;PowerOutput;Running;solarCurrent;solarVolt;Temp;TotalKwh;MaxACPower;FirmwareId;FirmwareRev,FirmwareDate;LinkStatus");
+            }
 
+
+
+            System.IO.File.AppendAllText(filename,
+                String.Format("{0};{1};{2};{3};{4};{5};{6};{7};{8};{9};{10};{11};{12};{13};{14}\r\n", DateTime.Now, z.Huidige.ACPower, z.Huidige.Activity, z.Huidige.mainsVoltage, z.Huidige.PowerOutput, z.Huidige.RunningHours, z.Huidige.solarCurrent, z.Huidige.solarVoltage, z.Huidige.Temperature, z.Huidige.TotalKwh,  z.Lange.MaxACPower, z.Lange.MaxACPower, z.Lange.FirmwareID, z.Lange.FirmwareRev, z.Lange.FirmwareDate, z.LinkStatus));
+
+            
             foreach (Zon.HistoricalData hd in z.Historisch)
             {
                 System.IO.File.WriteAllText(string.Format(ConfigurationManager.AppSettings["historicfile"], DateTime.Now),
